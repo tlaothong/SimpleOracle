9af3bef32eacdacf00f05b8d083342fa3f63f543
Teerachai Laothong <tlaothong@gmail.com>
2022-06-16 19:01:03 +0700

    Removed unused import

--- onchain/OracleSamples/contracts/SimEvent.sol
+++ onchain/OracleSamples/contracts/SimEvent.sol
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
-
 contract SimEvent {
 
     event MyEvent(uint num, string text);
